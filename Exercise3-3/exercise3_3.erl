c6575427a4c30ebb6af3541c3bd5cb0e42b1345f
ilya Sergeev <ilyasergeev@MacBook-Pro-ilya.local>
2013-01-08 14:02:46 +0400

    add function to task 3_3

--- Exercise3-3/exercise3_3.erl
+++ Exercise3-3/exercise3_3.erl
@@ -1,8 +1,16 @@
 -module (exercise3_3).
 
 -export ([print/1]).
+-export ([print_even/1]).
 
 print(1) -> io:format("Number:~p~n", [1]);
 print(N) when is_integer(N), N > 1 ->
 	print(N-1),
 	io:format("Number:~p~n", [N]).
+	
+print_even(1) -> ok;
+print_even(2) -> io:format("Number:~p~n", [2]);
+print_even(N) when is_integer(N), N rem 2 == 0 ->
+	print_even(N-2),
+	io:format("Number:~p~n", [N]);
+print_even(N) when is_integer(N) -> print(N-1).
