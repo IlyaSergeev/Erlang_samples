11b24f320563e31490bea0e856dd6f5792bc1d63
ilya Sergeev <ilyasergeev@MacBook-Pro-ilya.local>
2013-01-12 00:06:10 +0400

    fix sorting

--- Exercise 3-6/exercise3_6.erl
+++ Exercise 3-6/exercise3_6.erl
@@ -2,31 +2,41 @@
 
 -export ([qsort/1]).
 -export ([mergeSort/1]).
+-export ([test/0]).
 
 qsort([]) -> [];
-qsort([X|Tail]) -> 
-	More = qsort(moreBase(X, Tail)),
-	Less = qsort(lessBase(X, Tail)),
-	Less++[X]++More.
-
-moreBase(_BaseEl, []) -> [];
-moreBase(BaseEl, [X|Tail]) when X > BaseEl -> [X|moreBase(BaseEl, Tail)];
-moreBase(BaseEl, [_X|Tail]) -> moreBase(BaseEl, Tail).
-
-lessBase(_BaseEl, []) -> [];
-lessBase(BaseEl, [X|Tail]) when X =< BaseEl -> [X|lessBase(BaseEl, Tail)];
-lessBase(BaseEl, [_X|Tail]) -> lessBase(BaseEl, Tail).
+qsort([BaseEl|Tail]) -> 
+	{Less, More} = separate(BaseEl, Tail),
+	qsort(Less) ++ [BaseEl] ++ qsort(More).
+	
+separate(_BaseEl, []) -> {[], []};
+separate(BaseEl, [X|Tail]) ->
+	{Less, More} = separate(BaseEl, Tail),
+	case X > BaseEl of
+		true -> {Less, [X|More]};
+		false -> {[X|Less], More}
+	end.
 
 mergeSort([]) -> [];
 mergeSort([X|[]]) -> [X]; 
 mergeSort(List) -> 
-	Halfs = lists:split(length(List) div 2, List),
-	LeftHalf = mergeSort(element(1, Halfs)),
-	RightHalf = mergeSort(element(2, Halfs)),
-	merge(LeftHalf, RightHalf).
+	{LeftHalf, RightHalf} = lists:split(length(List) div 2, List),
+	merge(mergeSort(LeftHalf), mergeSort(RightHalf)).
 
 merge([], []) -> [];
 merge(List, []) -> List;
 merge([], List) -> List;
-merge([XL|TailL], [XR|TailR]) when XL > XR -> [XR|merge([XL|TailL], TailR)];
-merge([XL|TailL], [XR|TailR]) -> [XL|merge(TailL, [XR|TailR])].
+merge([XL|TailL], [XR|TailR]) ->
+	case XL > XR of
+		true -> [XR|merge([XL|TailL], TailR)];
+		false -> [XL|merge(TailL, [XR|TailR])]
+	end.
+
+test() ->
+	[] = qsort([]),
+	[0,1,2,3,4,5,6,7,8,9] = qsort([5,4,7,9,1,2,0,3,6,8]),
+	[1,1,1,2,2,2,3,3] = qsort([1,2,3,2,2,1,1,3]),
+	[] = mergeSort([]),
+	[0,1,2,3,4,5,6,7,8,9] = mergeSort([5,4,7,9,1,2,0,3,6,8]),
+	[1,1,1,2,2,2,3,3] = mergeSort([1,2,3,2,2,1,1,3]),
+	ok.
